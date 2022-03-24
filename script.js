48765f06f2c7a5e7a869fcdef3eb27fad28077ce
Mac <Mac@MacBook-Air.local>
2022-03-24 12:03:36 -0600

    adding new functions

--- script.js
+++ script.js
@@ -1,15 +1,28 @@
 const fetchPokemon = () => {
     const url = `https://pokeapi.co/api/v2/pokemon/pikachu`;
     let nombrePokemon = document.getElementById('nombrePokemon').value;
-    // console.log(nombrePokemon);
 
     fetch(url).then((res) => {
         console.log(res);
         return res.json();
     }).then((data) => {
         console.log(data);
+        let pokeName = data.name
+        setName(pokeName);
+        setType(data.types[0].type.name);
 
     })
+    
 
 
+}
+
+const setName = (nombre) => {
+    nombre = nombre.toUpperCase();
+    document.getElementById("pokeName").innerHTML= "El nombre del pokemon es: " + nombre;
+}
+
+const setType = (tipo) => {
+    tipo = tipo.toUpperCase();
+    document.getElementById("tipoPoke").innerHTML = "Tu pokemon es de tipo: " + tipo;
 }
