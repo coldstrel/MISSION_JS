cad352b89744f09eaa89b56c841abd5139fb9f49
coldstrel <aldofra98@gmail.com>
2022-03-25 12:09:34 -0600

    finally

--- script.js
+++ script.js
@@ -1,60 +1,76 @@
 const fetchPokemon = () => {
+    $('.moves').hide();
+    $('.abilities').hide();
+    const pokeName = document.getElementById('nombrePokemon').value;
 
-    let nombrePokemon = document.getElementById('nombrePokemon').value;
-    const url = `https://pokeapi.co/api/v2/pokemon/${nombrePokemon}`;
-
-
-
+    const url = `https://pokeapi.co/api/v2/pokemon/${pokeName}`;
     fetch(url).then((res) => {
-        console.log(res);
-        return res.json();
+        if (res.status != 200) {
+            poke_image("resources/pokebola.png");
+            clean_text();
+        } else {
+            return res.json();
+        }
     }).then((data) => {
-        console.log(data.name);
-        let pokeName = data.name
-        setName(pokeName);
-        setType(data.types[0].type.name);
-        setImg(data.sprites.front_default);
-        console.log(data)
-
-        // Getting the stats for the pokemon
-        var listStatsvalue = [];
-
-        for (var i in data.stats) {
-            var statValue = data.stats[i].base_stat;
-            listStatsvalue.push(statValue);
-        }
-
-        document.getElementById("1").innerHTML = listStatsvalue[0];
-        document.getElementById("2").innerHTML = listStatsvalue[1];
-        document.getElementById("3").innerHTML = listStatsvalue[2];
-        document.getElementById("4").innerHTML = listStatsvalue[3];
-        document.getElementById("5").innerHTML = listStatsvalue[4];
-        document.getElementById("6").innerHTML = listStatsvalue[5];
-        // Finishing getting the stats
-
-        var z = document.getElementById("movPoke");
-        for (var i = 0; i < data.abilities.length; i++) {
-            console.log(data.abilities[i].ability.name);
-            var div = document.createElement("p");
-            div.innerHTML = data.abilities[i].ability.name;
-            z.appendChild(div);
-        }
+        $('#type_info').text(data.types[0].type.name.toUpperCase());
+        poke_info(data.stats);
+        abilities(data.abilities);
+        moves(data.moves);
+        poke_image(data.sprites.front_default);
     })
-
-
-
 }
 
-const setName = (nombre) => {
-    nombre = nombre.toUpperCase();
-    document.getElementById("pokeName").innerHTML = "El nombre del pokemon es: " + nombre;
+const poke_info = (stats) => {
+    $('#hp').text(`HP: ${stats[0].base_stat}`);
+    $('#atk').text(`ATK: ${stats[1].base_stat}`);
+    $('#def').text(`DEF: ${stats[2].base_stat}`);
+    $('#speed').text(`SPD: ${stats[5].base_stat}`);
+    $('.basic_info').show();
 }
 
-const setType = (tipo) => {
-    tipo = tipo.toUpperCase();
-    document.getElementById("tipoPoke").innerHTML = "Tu pokemon es de tipo: " + tipo;
+const abilities = (abilities) => {
+    $('#first_ability').text(abilities[0].ability.name);
+    if (abilities[1]) {
+        $('#second_ability').text(abilities[1].ability.name);
+    }
+    if (abilities[2]) {
+        $('#hidden_ability').text(abilities[2].ability.name);
+    }
 }
 
-const setImg = (img) => {
-    document.getElementById("pokeImg").src = img;
+const moves = (moves) => {
+    console.log("entraste a movs")
+    $('#first_move').text(`1.- ${moves[0].move.name}`);
+    $('#second_move').text(`2.- ${moves[1].move.name}`);
+    $('#third_move').text(`3.- ${moves[2].move.name}`);
 }
+
+const poke_image = (url) => {
+    const poke_img = document.getElementById("pokeImg")
+    poke_img.src = url
+}
+
+const clean_text = () => {
+    $('#hp').text('');
+    $('#atk').text('');
+    $('#def').text('');
+    $('#speed').text('');
+    $('#name').text('');
+    $('#type_info').text('');
+}
+
+$('#abilities').on('click', function() {
+
+    $('.basic_info').hide();
+    $('.moves').hide();
+    $('.abilities').show();
+
+});
+
+$('#moves').on('click', function() {
+
+    $('.basic_info').hide();
+    $('.abilities').hide();
+    $('.moves').show();
+
+});
